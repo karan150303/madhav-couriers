8624332a50570c2daab66f80d5bee74eecb8e100
karan150303 <karanthakur00187@gmail.com>
2025-06-20 15:31:45 +0530

    admin.js

--- api/models/admin.js
+++ api/models/admin.js
@@ -1,1 +1,36 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
+const AdminSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 3,
+        maxlength: 20
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// Hash password before saving
+AdminSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+    
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (err) {
+        next(err);
+    }
+});
+
+module.exports = mongoose.model('Admin', AdminSchema);
